f4dff06a2f2dbe5ad128793e1245c2fcbb26b886
Philopateer Ghaly <philogeee@gmail.com>
2025-07-20 13:07:56 +0200

    cam feed now uses response -> Need a way to switch feeds

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 import dash
+from dash import dcc, html
 import flask
+from flask import Flask, Response
+import dash_bootstrap_components as dbc
 from quart import Quart, websocket
 import cv2
 from rich import print
 # from datetime import datetime, timedelta
 from dash_bootstrap_components.themes import LUX as THEME
 
+import threading
 # from src import definitions
 
 # Configure the app
@@ -15,7 +19,7 @@ app = dash.Dash(
     server=flask.Flask(__name__),
     title="Cams 4 Niels",
     external_stylesheets=[THEME],
-    use_pages=True,
+    # use_pages=True,
     meta_tags=[
         {"charset": "utf-8"},
         {"name": "viewport", "content": "width=device-width, initial-scale=1, shrink-to-fit=no"},
@@ -24,13 +28,145 @@ app = dash.Dash(
 
 server = app.server
 
+
+CAMS_LABELS = {
+    'Zone': [37, 38, 39, 41, 45, 52, 54, 56, 57, 59, 60, 61, 67],
+    'Platform': [35, 40, 43, 51, 57, 60, 62, 63, 65, 66, 68],
+    'Equipment' : [42, 44, 53, 69],
+    'Helium': [39, 40, 61, 67 ],
+    'Rooms' : [34, 36, 47, 48, 64, 69,]
+}
+
+
+def get_camera_rtsp_path(cam_id):
+    # ! Unfortunately, needs to be hardcoded.
+    
+    if 34 <= int(cam_id) <= 43:
+        return f"rtsp://alphacam:maxalpha@alphacam{cam_id}.cern.ch/stream1"
+    elif 44 <= int(cam_id) <= 48:
+        return f"rtsp://alphacam:Maxalpha@alphacam{cam_id}.cern.ch/stream1"
+    elif int(cam_id) >= 50:
+        return f"rtsp://alpha-admin:Nmt30smiAg$@alphacam{cam_id}.cern.ch/stream1"
+    else:
+        return None
+
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+class VideoCamera(object):
+    def __init__(self, camera_rtsp):
+        self.video = cv2.VideoCapture(camera_rtsp)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+
+# def create_layout(App: dash.Dash, Server: Flask) -> dash.html.Div:
+    
+    
+@server.route('/video_feed/<cam_id>')
+def video_feed(cam_id):
+    rtsp_url = get_camera_rtsp_path(cam_id)
+    if not rtsp_url:
+        return "Camera not found", 404
+    return Response(gen(VideoCamera(rtsp_url)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def camera_card(cam_id):
+    return dbc.Card(
+        [
+            dbc.CardImg(src=f"/video_feed/{cam_id}", top=True, style={"height": "300px", "objectFit": "cover"}),
+        ],
+        style={"width": "22rem", "margin": "10px"},
+    )
+
+# layout = dbc.Container(
+#     [
+#         html.H1("Webcam Feeds", style={"textAlign": "center", "margin": "20px"}),
+#         dbc.Row(
+#             [
+#                 dbc.Col(camera_card("41")),
+#                 dbc.Col(camera_card("35")),
+#                 dbc.Col(camera_card("56")),
+#             ],
+#             justify="center",
+#         ),
+#     ],
+#     fluid=True,
+# )
+
+dropdown_menu = dcc.Dropdown(
+    id="camera-label-dropdown",
+    options=[{"label": label, "value": label} for label in CAMS_LABELS.keys()],
+    value=list(CAMS_LABELS.keys())[0],
+    style={"width": "300px", "margin": "20px auto"}
+)
+
+
+
+@app.callback(
+    dash.Output("camera-mosaic", "children"),
+    [dash.Input("camera-label-dropdown", "value"),
+     dash.Input("interval-component", "n_intervals")
+        ]
+)
+def update_mosaic(selected_label):
+    
+    # Close any opened cameras
+    
+    # Shut down all open video feeds
+    print(f"Selected label: {selected_label}")
+    cam_ids = CAMS_LABELS.get(selected_label, [])
+    print(f"Camera IDs: {cam_ids}")
+    if not cam_ids:
+        return html.Div("No cameras found for this label.")
+    
+    cards = [dbc.Col(camera_card(str(cam_id)), width=4) for cam_id in cam_ids]
+    print(f"Generated cards")
+    return dbc.Row(cards, justify="center")
+
+
+layout = html.Div(
+    [
+        html.H2("Camera Area", style={"textAlign": "center", "margin": "20px"}),
+        dropdown_menu,
+        html.Div(id="camera-mosaic"),
+        # dcc.Interval(id="interval-component", interval=1000, n_intervals=0)
+    ]
+)
+
+app.layout = layout
+
+if __name__ == '__main__':
+    threading.Thread(target=app.run).start()
+    server.run()
+
+
+
+
+
+
 # Setup pages
-import layouts
-from pages import home_page
+# import layouts
+# from pages import home_page
 
-app.layout = layouts.main_layout.create_layout(app)
+# app.layout = layouts.main_layout.create_layout(app)
 
-home_page.layout.children = layouts.home_layout.create_layout(app, server)
+# home_page.layout.children = layouts.home_layout.create_layout(app, server)
+
+
 
 
 
