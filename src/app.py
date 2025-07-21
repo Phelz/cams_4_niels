4ec553afc19d1f26b25a73ad6127890d2064c773
Philopateer Ghaly <philogeee@gmail.com>
2025-07-21 12:03:22 +0200

    refactored app.py -> Need to refactor others + add other cameras logic

--- src/app.py
+++ src/app.py
@@ -1,20 +1,16 @@
-from rich import print
-
 import asyncio
 import base64
 import cv2
 import threading
 
-from quart import Quart, websocket, Response, send_from_directory
+from quart import Quart, websocket
 import dash
-from dash import dcc, html, Output, Input
-import dash_bootstrap_components as dbc
 from dash_bootstrap_components.themes import LUX as THEME
 
 from collections import defaultdict
+from rich import print
 import time
 
-# === DASH + QUART SETUP ===
 CAMS_LABELS = {
     'Zone': [37, 38, 39, 41, 45, 52, 54, 56, 57, 59, 60, 61, 67],
     'Platform': [35, 40, 43, 51, 57, 60, 62, 63, 65, 66, 68],
@@ -23,8 +19,13 @@ CAMS_LABELS = {
     'Rooms' : [34, 36, 47, 48, 64, 69,]
 }
 
-def get_camera_rtsp_path(cam_id):
-    # ! Unfortunately, needs to be hardcoded.
+DELAY_BETWEEN_FRAMES = 0.05 # ! add delay (in seconds) if CPU usage is too high
+
+
+def get_camera_rtsp_path(cam_id:int) -> str:
+    ''' Get the RTSP path for a given camera ID.'''
+    
+    # ! Unfortunately, needs to be hardcoded, since not all use maxalpha.
     if 34 <= int(cam_id) <= 43:
         return f"rtsp://alphacam:maxalpha@alphacam{cam_id}.cern.ch/stream1"
     elif 44 <= int(cam_id) <= 48:
@@ -34,22 +35,12 @@ def get_camera_rtsp_path(cam_id):
     else:
         return None
     
-# class VideoCamera(object):
-#     def __init__(self, video_path):
-#         self.video = cv2.VideoCapture(video_path)
 
-#     def __del__(self):
-#         self.video.release()
-
-#     def get_frame(self):
-#         success, image = self.video.read()
-#         if not success:
-#             print(f"[WARN] Failed to read frame from camera {self.video}")
-#             return b''
-#         _, jpeg = cv2.imencode('.jpg', image)
-#         return jpeg.tobytes()
 class CameraStreamManager:
+    ''' Manages camera streams and frames in a thread-safe manner. '''
+    
     def __init__(self):
+        ''' Initialize the camera stream manager. '''
         self.frames = {}  # cam_id -> latest frame (JPEG bytes)
         self.locks = defaultdict(threading.Lock) # thread-safe access to frames
         self.threads = {} # cam_id -> thread
@@ -83,32 +74,21 @@ class CameraStreamManager:
         with self.locks[cam_id]:
             return self.frames.get(cam_id, b'')
 
-camera_manager = CameraStreamManager()
 
 # Setup small Quart server for streaming via websocket.
 server = Quart(__name__)
-delay_between_frames = 0.05 # add delay (in seconds) if CPU usage is too high
-
-
-# @server.websocket("/video_feed/<cam_id>")
-# async def stream(cam_id):
-#     camera = VideoCamera( get_camera_rtsp_path(cam_id) )  # zero means webcam
-#     while True:
-#         if delay_between_frames is not None:
-#             await asyncio.sleep(delay_between_frames)  # add delay if CPU usage is too high
-#         frame = camera.get_frame()
-#         await websocket.send(f"data:image/jpeg;base64, {base64.b64encode(frame).decode()}")
-
+camera_manager = CameraStreamManager()
 
 @server.websocket("/video_feed/<cam_id>")
 async def stream(cam_id):
     ''' Stream video frames for a given camera ID via WebSocket. Uses Asyncio for non-blocking I/O. '''
+    
     cam_id = int(cam_id)
     rtsp_url = get_camera_rtsp_path(cam_id)
     camera_manager.start_camera_thread(cam_id, rtsp_url)
 
     while True:
-        await asyncio.sleep(delay_between_frames)
+        await asyncio.sleep(DELAY_BETWEEN_FRAMES)
         frame = camera_manager.get_frame(cam_id)
         if frame:
             await websocket.send(f"data:image/jpeg;base64,{base64.b64encode(frame).decode()}")
@@ -128,32 +108,10 @@ app = dash.Dash(
     ],
 )
 
-# Create a clientside callback for each camera to update the image source
-# for key, cam_ids in CAMS_LABELS.items():
-#     for cam_id in cam_ids:
-#         app.clientside_callback(
-#             f"function(m){{return m? m.data : '';}}",
-#             Output(f"video-{cam_id}", "src"),
-#             Input(f"ws-{cam_id}", "message")
-#         )
-
-
-
 # Dash multipage container
 import layouts.main_layout
-import components
-from pages import home_page, zone_page, platform_page
 app.layout = layouts.main_layout.create_layout(app)
 
-
-
-
-# # Register pages (example)
-# home_page.layout = layouts.home_layout.create_layout(app, server)
-# zone_page.layout = layouts.zone_layout.create_layout(app, server)
-# platform_page.layout = layouts.platform_layout.create_layout(app, server)
-
-# === RUN APP ===
 
 if __name__ == "__main__":
     threading.Thread(target=app.run).start()
